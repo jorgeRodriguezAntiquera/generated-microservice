1038ee7d9441821c141b3f6e7418d4125dd26e54
Jorge Rodriguez <jorge.rodrigueza@alumnos.uv.cl>
2023-07-09 22:42:20 -0400

    Generated microservicetestt13_v1_09/07/2023_22-42-20

---
+++ src/main/java/com/demo/spring/jpa/msGenerate/repository/TutorialDetailRepository.java
@@ -0,0 +1,8 @@
+package com.demo.spring.jpa.msGenerate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.demo.spring.jpa.msGenerate.model.*;
+
+@Repository
+public interface TutorialDetailRepository extends JpaRepository<TutorialDetail, Long>{}
